9e8449c0d65b3fa466713ae1c8de2a10997bbc58
vishrutss <vishrutss@gmail.com>
2023-04-17 09:13:31 -0700

    Updated homework 2 to be a library crate

---
+++ homework_2/target/debug/deps/homework_2-d46fb61322507b96.d
@@ -0,0 +1,5 @@
+d:\Rust_Projects\rust-homeworks\homework_2\target\debug\deps\homework_2-d46fb61322507b96.rmeta: src\lib.rs
+
+d:\Rust_Projects\rust-homeworks\homework_2\target\debug\deps\homework_2-d46fb61322507b96.d: src\lib.rs
+
+src\lib.rs:
--- homework_2/target/debug/homework_2.d
+++
@@ -1,1 +0,0 @@
-D:\Rust_Projects\rust-homeworks\homework_2\target\debug\homework_2.exe: D:\Rust_Projects\rust-homeworks\homework_2\src\main.rs
